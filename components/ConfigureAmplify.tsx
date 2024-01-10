2b1bf4ad4cad43857cb1aeda57818ce2e5b63ef3
briggsrr <robert.briggs.3@icloud.com>
2024-01-09 23:49:19 -0800

    rb - trying todo test

---
+++ components/ConfigureAmplify.tsx
@@ -0,0 +1,17 @@
+// app/page.tsx
+"use client";
+
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import TodoList from "@/components/TodoList";
+
+function App() {
+  return (
+    <>
+      <h1>Derm IQ test</h1>
+      <TodoList />
+    </>
+  );
+}
+
+export default withAuthenticator(App);
---
+++ components/TodoList.tsx
@@ -0,0 +1,34 @@
+// components/TodoList.tsx
+"use client";
+
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+
+// generate your data client using the Schema from your backend
+const client = generateClient<Schema>();
+
+export default function TodoList() {
+  const [todos, setTodos] = useState<Schema["Todo"][]>([]);
+
+  async function listTodos() {
+    // fetch all todos
+    const { data } = await client.models.Todo.list();
+    setTodos(data);
+  }
+
+  useEffect(() => {
+    listTodos();
+  }, []);
+
+  return (
+    <div>
+      <h1>Todos</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
